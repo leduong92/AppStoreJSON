72620b638277e78e4257b5c4e3a081c25a3db32d
Mai Le Duong <macbook@MaiLeDuong.local>
2019-07-31 20:55:21 +0700

    Present daily list fullscreen

--- AppStoreJSONApis/Controller/Today/TodayMultipleAppsController.swift
+++ AppStoreJSONApis/Controller/Today/TodayMultipleAppsController.swift
@@ -14,25 +14,51 @@ class TodayMultipleAppsController: BaseListController, UICollectionViewDelegateF
     
     var results = [FeedResult]()
     
+    
+    let closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "close_button"), for: .normal)
+        button.tintColor = .darkGray
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleDismiss() {
+        dismiss(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if mode == .fullscreen {
+            setupCloseButton()
+        } else {
+            collectionView.isScrollEnabled = false
+        }
+        
         collectionView.backgroundColor = .white
-        collectionView.isScrollEnabled = false
-        
         collectionView.register(MultipleAppCell.self, forCellWithReuseIdentifier: cellId)
-        
-        // never execute fetch code inside of a view
-//        Service.shared.fetchGames { (appGroup, err) in
-//            self.results = appGroup?.feed.results ?? []
-//
-//            DispatchQueue.main.async {
-//                self.collectionView.reloadData()
-//            }
-//        }
+    }
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    fileprivate func setupCloseButton() {
+        view.addSubview(closeButton)
+        closeButton.anchor(top: view.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 20, left: 0, bottom: 0, right: 16), size: .init(width: 44, height: 44))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if mode == .fullscreen {
+            return .init(top: 12, left: 24, bottom: 12, right: 24)
+        }
+        return .zero
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if mode == .fullscreen {
+            return results.count
+        }
+        
         return min(4, results.count) // toi da 4 row
     }
     
@@ -44,7 +70,11 @@ class TodayMultipleAppsController: BaseListController, UICollectionViewDelegateF
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let height: CGFloat = (view.frame.height - 3 * spacing) / 4
+        let height: CGFloat = 68
+        
+        if mode == .fullscreen {
+            return .init(width: view.frame.width - 48, height: height)
+        }
         
         return .init(width: view.frame.width, height: height)
     }
@@ -55,5 +85,19 @@ class TodayMultipleAppsController: BaseListController, UICollectionViewDelegateF
         return spacing
     }
     
+    fileprivate let mode: Mode
+    
+    enum Mode {
+        case small, fullscreen
+    }
+    
+    init(mode: Mode) {
+        self.mode = mode
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
