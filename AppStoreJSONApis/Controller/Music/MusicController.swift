dd5fdd1ff1bb4b40e28c5c804e85b5ea015f99c1
Mai Le Duong <macbook@MaiLeDuong.local>
2019-08-02 21:02:20 +0700

    track

---
+++ AppStoreJSONApis/Controller/Music/MusicController.swift
@@ -0,0 +1,48 @@
+//
+//  MusicController.swift
+//  AppStoreJSONApis
+//
+//  Created by Mai Le Duong on 8/2/19.
+//  Copyright © 2019 Mai Le Duong. All rights reserved.
+//
+
+import UIKit
+//1. Implement Cell
+//2. Implement footer
+class MusicController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellId = "cellId"
+    fileprivate let footerId = "footerId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(TrackCell.self, forCellWithReuseIdentifier: cellId)
+        
+        collectionView.register(MusicLoadingFooter.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerId)
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: cellId, for: indexPath)
+        
+        return footer
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TrackCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
